86ec9f24583d26ef4c2e6201f6b95b9ef51fa4f2
Vudang <danglegiavu@gmail.com>
2021-03-11 23:08:21 +0700

    add product model

---
+++ models/Product.js
@@ -0,0 +1,47 @@
+import { Schema as _Schema, model } from 'mongoose';
+const Schema = _Schema;
+
+const productSchema = new Schema(
+  {
+    filename: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    price: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+      required: true,
+    },
+    origin: {
+      type: String,
+      required: true,
+    },
+    standard: {
+      type: String,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      default: '',
+    },
+    thumb: {
+      type: String,
+      default: '',
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+const Product = model('product', productSchema);
+
+export default Product;
