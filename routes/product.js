6c5cd4ff7d8957db9d063af15a31d8ef959c4c54
Vudang <danglegiavu@gmail.com>
2021-03-12 23:27:13 +0700

    add upload and resize with multer

--- routes/product.js
+++ routes/product.js
@@ -2,8 +2,13 @@ import express from 'express';
 const ROUTER = express.Router();
 
 import * as PRODUCT_CONTROLLER from '../controllers/product'
+import { resize, upload } from '../middlewares/upload';
 
 ROUTER.get('/',PRODUCT_CONTROLLER.GET_LIST_PRODUCTS)
 
-ROUTER.post('/',PRODUCT_CONTROLLER.CREATE_PRODUCT)
+ROUTER.post(
+  '/',upload.single('filename'),
+  resize,
+  PRODUCT_CONTROLLER.CREATE_PRODUCT);
+
 export default ROUTER;
