d760f0ce27ec7624dc3416deebf91c5109b0ae10
Vudang <danglegiavu@gmail.com>
2021-03-13 18:31:17 +0700

    add verify token middleware

---
+++ middlewares/verifyToken.js
@@ -0,0 +1,16 @@
+import { verify } from "jsonwebtoken";
+
+const auth = (req, res, next) => {
+  const token = req.header("auth-token");
+  if (!token) {
+    return res.status(401).send({ err: "Token is expired" });
+  }
+  try {
+    verify(token, process.env.SECRET_TOKEN);
+    next();
+  } catch (err) {
+    res.status(400).send({ err: "Invalid Token" });
+  }
+};
+
+export default auth;
