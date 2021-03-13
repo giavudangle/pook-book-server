bbcf71b8c199aa54da9d338605fdb0c57925ec2e
Vudang <danglegiavu@gmail.com>
2021-03-13 17:23:30 +0700

    add user model

---
+++ models/User.js
@@ -0,0 +1,38 @@
+import { Schema as _Schema, model } from 'mongoose';
+const Schema = _Schema;
+
+const registerSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    lowercase: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    // enum: 10,
+    default: '',
+  },
+  address: {
+    type: String,
+    // minlength: 6,
+    default: '',
+  },
+  profilePicture: {
+    type: String,
+  },
+  pushTokens: {
+    type: Array,
+    required: true,
+  },
+});
+
+const Register = model('user', registerSchema);
+export default Register;
