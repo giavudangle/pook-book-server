0bf9dd641184791c2be8b688743eae74e12a1b39
Vudang <danglegiavu@gmail.com>
2021-03-16 01:23:50 +0700

    update size image email return

--- middlewares/email.js
+++ middlewares/email.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 import nodemailer from 'nodemailer'
 
+const host = process.env.HOST_NAME
 
 const transporter = nodemailer.createTransport({
   host:"smtp.gmail.com",
@@ -13,6 +14,7 @@ const transporter = nodemailer.createTransport({
   }
 })
 
+// verify connection configuration
 // transporter.verify((err,succ) => {
 //   if(err){
 //     console.log('====================================');
@@ -25,10 +27,9 @@ const transporter = nodemailer.createTransport({
 //   }
 // })
 
-// verify connection configuration
 
 const getPasswordResetURL = (user, token) =>
-  `http://192.168.0.27:8080/expo?userid=${user._id}&token=${token}`;
+  `http://${host}:8080/expo?userid=${user._id}&token=${token}`;
 
 const resetPasswordTemplate = (user, url) => {
   const from = process.env.EMAIL_LOGIN;
@@ -43,7 +44,7 @@ const resetPasswordTemplate = (user, url) => {
   <p>If you don't want to change your password, please ignore and delete this message! </p>
   <p>Thank you,</p>
   <p>Your friend CodingwithVudang 🚀</p>
-  <img src="https://scontent.fsgn2-1.fna.fbcdn.net/v/t1.0-9/151284066_207254161142817_5812038792384707893_n.png?_nc_cat=105&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=pRGC33VnrYgAX8pPhJm&_nc_ht=scontent.fsgn2-1.fna&oh=df9a2f46200a1047c3ef4cfb402f245e&oe=6071E763" alt="logo" width="500" height="160" > 
+  <img src="https://scontent.fsgn2-1.fna.fbcdn.net/v/t1.0-9/151284066_207254161142817_5812038792384707893_n.png?_nc_cat=105&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=pRGC33VnrYgAX8pPhJm&_nc_ht=scontent.fsgn2-1.fna&oh=df9a2f46200a1047c3ef4cfb402f245e&oe=6071E763" alt="logo" width="500" height="500" > 
   `;
 
   return { from, to, subject, html };
@@ -60,7 +61,7 @@ const registerUserTemplate = (user) => {
   <p>If you have any questions please contact support</p>
   <p>Best regards,</p>
   <p>Your friend CodingwithVudang 🚀</p>
-  <img src="https://scontent.fsgn2-1.fna.fbcdn.net/v/t1.0-9/151284066_207254161142817_5812038792384707893_n.png?_nc_cat=105&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=pRGC33VnrYgAX8pPhJm&_nc_ht=scontent.fsgn2-1.fna&oh=df9a2f46200a1047c3ef4cfb402f245e&oe=6071E763" alt="logo" width="500" height="60" > 
+  <img src="https://scontent.fsgn2-1.fna.fbcdn.net/v/t1.0-9/151284066_207254161142817_5812038792384707893_n.png?_nc_cat=105&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=pRGC33VnrYgAX8pPhJm&_nc_ht=scontent.fsgn2-1.fna&oh=df9a2f46200a1047c3ef4cfb402f245e&oe=6071E763" alt="logo" width="500" height="500" > 
   `;
 
   return { from, to, subject, html };
