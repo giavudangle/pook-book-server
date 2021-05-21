3f79368e0db408601f25072725f3933ad8e89f7e
Vudang <danglegiavu@gmail.com>
2021-05-22 00:17:49 +0700

    populate favorite in get method

--- controllers/favorite/index.js
+++ controllers/favorite/index.js
@@ -3,7 +3,30 @@ import Favorite from "../../models/Favorite";
 
 const GetFavorites = (req, res) => {
   Favorite.find()
-    .populate('items.item')
+    .populate({
+      path:'items.item',
+      populate:{
+        path:'author',
+      }
+    })
+    .populate({
+      path:'items.item',
+      populate:{
+        path:'category',
+      }
+    })
+    .populate({
+      path:'items.item',
+      populate:{
+        path:'provider',
+      }
+    })
+    .populate({
+      path:'items.item',
+      populate:{
+        path:'publisher',
+      }
+    })
     .then((data) => {
       return res.status(200).send({
         status: "OK",
@@ -30,10 +53,30 @@ const PostFavorite = (req, res) => {
   }
   Favorite.findOne({ userId: req.body.userId }, (err, result) => {
     if (err) {
-      console.log(err);
+      return res.status(404).send({
+        status: "ERR_SERVER",
+        message: err.message,
+        data: null,
+      });
     }
+    console.log(result);
+    // {
+    //   _id: 6087c9ca66a27d4230f61d9e,
+    //   userId: '6081a65c13f3e21db7724b3a',
+    //   items: [ null ],
+    //   createdAt: 2021-04-27T08:22:34.050Z,
+    //   updatedAt: 2021-05-16T08:11:06.819Z,
+    //   __v: 5
+    // }
+
+    // {
+    //   userId: '6081a65c13f3e21db7724b3a',
+    //   items: [ { item: '6088e6fa83287d03fb4b2380' } ]
+    // }
+
+
     if (result) {
-      result.items.push(req.body.items[0].item);
+      result.items.push(req.body.items[0]);
       result
         .save()
         .then((data) => {
